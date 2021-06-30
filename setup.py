f5cd9218b8044c28666bb150d76bdf2eb8c44d0e
Srikanth Kandarp <48326585+Srikanth-Kandarp@users.noreply.github.com>
2021-06-30 20:10:26 +0530

    Updated pypi code

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Education",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+]
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="Woolly-lib",
+    version="0.0.1",
+    description="This Library for pytorch based utilities which will be used for training and visualizing cv models",
+    long_description=open("README.txt").read() + "\n\n" + open("CHANGELOG.txt").read(),
+    url="",
+    author="Team_woolly",
+    author_email="srikanthakandarp23@gmail.com",
+    license="MIT",
+    classifiers=classifiers,
+    keywords="ML",
+    packages=find_packages(),
+    install_requires=required,
+)
