8eafde387598a8b4f98e51421c7e93bea502c0a1
Srikanth Kandarp <48326585+Srikanth-Kandarp@users.noreply.github.com>
2021-07-05 13:46:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="woolylib",
-    version=woolylib_version,
+    version=woollylib_version,
     author="Team_woolly",
     author_email="srikanthakandarp23@gmail.com",
     description="This Library for pytorch based utilities which will be used for training and visualizing cv models",
