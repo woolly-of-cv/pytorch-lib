c40bfb95e8aaaf70f0e4779e1201bb0b676f4163
Srikanth Kandarp <48326585+Srikanth-Kandarp@users.noreply.github.com>
2021-07-05 13:37:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,40 @@
-from setuptools import setup, find_packages
+import setuptools
+import subprocess
 import os
-
-classifiers = [
-    "Development Status :: 5 - Production/Stable",
-    "Intended Audience :: Education",
-    "License :: OSI Approved :: MIT License",
-    "Programming Language :: Python :: 3",
-]
 
 with open("requirements.txt") as f:
     required = f.read().splitlines()
 
-setup(
-    name="woollylib",
-    version="0.1.0",
-    description="This Library for pytorch based utilities which will be used for training and visualizing cv models",
-    url="",
+cf_remote_version = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
+assert "." in cf_remote_version
+
+assert os.path.isfile("cf_remote/version.py")
+with open("cf_remote/VERSION", "w", encoding="utf-8") as fh:
+    fh.write(f"{cf_remote_version}\n")
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="woolylib",
+    version=cf_remote_version,
     author="Team_woolly",
     author_email="srikanthakandarp23@gmail.com",
-    license="MIT",
-    classifiers=classifiers,
-    keywords="ML",
-    packages=find_packages(),
+    description="This Library for pytorch based utilities which will be used for training and visualizing cv models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/woolly-of-cv/pytorch-lib",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
     install_requires=required,
 )
