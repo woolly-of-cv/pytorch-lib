b7e0bd5abfb55fe1cb9989e6f7ff39acca089fbd
Ammar <ammaradil80@gmail.com>
2021-07-09 21:54:23 +0530

    Enable Nestrove

--- woollylib/bp/optimizer/backpropagation.py
+++ woollylib/bp/optimizer/backpropagation.py
@@ -6,4 +6,4 @@ torch.manual_seed(1)
 
 
 def get_sgd_optimizer(model, lr, momentum=0, weight_decay=0):
-    return SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
+    return SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay, nesterov=True)
