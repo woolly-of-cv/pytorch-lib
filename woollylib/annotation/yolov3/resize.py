d189ec4d11d2eb3c061f261dcfc7aabb36e55e4e
Ammar Adil <ammaradil80@gmail.com>
2021-07-23 20:48:20 +0530

    Annotated Images

--- woollylib/annotation/yolov3/resize.py
+++ woollylib/annotation/yolov3/resize.py
@@ -3,11 +3,12 @@ fixed_height = 800
 import glob
 from PIL import Image
 
-print(glob.glob('Images'+'/*/**.jp*g'))
-
-for im in glob.glob('Images'+'/*/**.jp*g'):
-    image = Image.open(im)
-    height_percent = (fixed_height / float(image.size[1]))
-    width_size = int((float(image.size[0]) * float(height_percent)))
-    image = image.resize((width_size, fixed_height), Image.BILINEAR)
-    image.save(im)
+for im in glob.glob('./Images'+'/**.jp*g'):
+    try:
+        image = Image.open(im)
+        height_percent = (fixed_height / float(image.size[1]))
+        width_size = int((float(image.size[0]) * float(height_percent)))
+        image = image.resize((width_size, fixed_height), Image.BILINEAR)
+        image.save(im)
+    except:
+        print(f"Error in Processing: {im}")
